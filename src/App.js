d667098c381b18a0ff681d3f1a56a43602303ebc
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-03-18 03:53:17 -0400

    Removed unecessary app wrapping and reapplied styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import HomePage from './Pages/HomePage'
 
 function App() {
   return (
-    <div className="App">
       <HomePage />
-    </div>
   );
 }
 
