e84d1ddb3ddde27dbd9e2adbc43d98c2f392650c
aalexcomas11 <comasrent@yahoo.com>
2020-04-17 11:55:43 -0400

    added about back

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import HomePage from './Pages/HomePage'
 import "./App.sass"
+import AboutSection from './Components/About'
 
 function App() {
   return (
       <div className="background">
         <HomePage />
-        {/* <AboutSection /> */}
+        <AboutSection />
       </div>
   );
 }
