ecaf8f22ec3fee312463ca1dcce266021f08ecbb
DaJuan Harris <dajuancancode@gmail.com>
2020-05-09 20:33:19 -0400

    Developed all breakpoints for faq section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './Pages/HomePage'
 
 import AboutSection from './Components/About/AboutSection'
 import TrackSection from './Components/TrackSection/TrackSection'
-import FAQ from './Components/FAQ/FAQ'
+import FAQSection from './Components/FAQSection/FAQSection'
 
 import "./App.sass"
 
@@ -13,12 +13,7 @@ function App() {
         <HomePage />
         <AboutSection />
         <TrackSection />
-        <FAQ 
-          name="What is a hackathon" 
-          body="A hackathon is a weekend-long event where students come together to learn about the latest technologies and build innovative products. Hackathons also provide opportunities for students by giving them access to various workshops, fun activities and networking events. " />
-         <FAQ 
-          name="What is a hackathon" 
-          body="A hackathon is a weekend-long event where students come together to learn about the latest technologies and build innovative products. Hackathons also provide opportunities for students by giving them access to various workshops, fun activities and networking events. " />
+        <FAQSection />
       </div>
   );
 }
---
+++ src/Components/FAQSection/FAQSection.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+import FAQ from "../FAQ/FAQ"
+import styles from "./FAQSection.module.sass"
+
+const FAQSection = props => {
+  
+  const questions = [1, 2, 3, 4, 5, 6, 7]
+
+  return(
+    <div className={styles.FAQSection}>
+      <h1 className={styles.FAQSection__title}>Frequently Asked Questions (FAQ)</h1>
+      <div className={styles.FAQSection__container}>
+        {questions && questions.map((question, index) => {
+          return (
+            <FAQ name="What is a hackathon" 
+            body={
+              `A hackathon is a weekend-long event where students come together to 
+              learn about the latest technologies and build innovative products. 
+              Hackathons also provide opportunities for students by giving them access to various workshops, 
+              fun activities and networking events.`
+            }/>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default FAQSection
